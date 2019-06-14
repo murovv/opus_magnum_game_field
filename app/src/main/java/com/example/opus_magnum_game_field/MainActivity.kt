6318445b0a3b697adf2a860bcc3745dbcca0f260
murovv <murovgleb@gmail.com>
2019-06-14 10:38:44 +0300

    init

---
+++ app/src/main/java/com/example/opus_magnum_game_field/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.opus_magnum_game_field
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

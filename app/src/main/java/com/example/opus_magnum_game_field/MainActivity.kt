e88efbfed2353ff5c6e9149c27ab18c170929131
murovv <murovgleb@gmail.com>
2019-06-14 14:17:23 +0300

    creatimg reactives

--- app/src/main/java/com/example/opus_magnum_game_field/MainActivity.kt
+++ app/src/main/java/com/example/opus_magnum_game_field/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.opus_magnum_game_field
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
+
+
+enum class ReagentsName{
+    //какие реагенты
+}
+
+open class Reagent(var name:ReagentsName, var amount:Int){
+    fun getImage(name: ReagentsName){
+    }
+
+}
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

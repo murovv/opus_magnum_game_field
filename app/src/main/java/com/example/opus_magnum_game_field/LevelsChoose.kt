14d8994a63b56d231b7e3ba75e79b7b4e642a23b
murovv <murovgleb@gmail.com>
2019-06-14 16:52:06 +0300

    рабочий макет c переходами между активностями

--- app/src/main/java/com/example/opus_magnum_game_field/LevelsChoose.kt
+++ app/src/main/java/com/example/opus_magnum_game_field/LevelsChoose.kt
@@ -1,12 +1,18 @@
 package com.example.opus_magnum_game_field
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_levels_choose.*
 
 class LevelsChoose : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_levels_choose)
+        button_lvl1.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/opus_magnum_game_field/MainMenu.kt
+++ app/src/main/java/com/example/opus_magnum_game_field/MainMenu.kt
@@ -1,11 +1,13 @@
 package com.example.opus_magnum_game_field
 
+import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main1.*
 
-class MainActivity1 : AppCompatActivity() {
+class MainMenu : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +19,9 @@ class MainActivity1 : AppCompatActivity() {
         val height = Resources.getSystem().displayMetrics.heightPixels
 
         val variable = 0
+        choose_level.setOnClickListener {
+            val intent = Intent(this,LevelsChoose::class.java)
+            startActivity(intent)
+        }
     }
 }

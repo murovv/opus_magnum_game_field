daac0b31b4b1db0fc44cb03f672deaefeb190f2a
murovv <murovgleb@gmail.com>
2019-06-16 18:05:46 +0300

    рабочий макет c переходами между активностями

--- app/src/main/java/com/example/opus_magnum_game_field/GameField.kt
+++ app/src/main/java/com/example/opus_magnum_game_field/GameField.kt
@@ -1,19 +1,35 @@
 package com.example.opus_magnum_game_field
 
 import android.content.Intent
+import android.content.res.Resources
+import android.content.res.XmlResourceParser
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Adapter
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
-
-enum class ReagentsName{
-    //какие реагенты
-}
-
-open class Reagent(var name:ReagentsName, var amount:Int){
-    fun getImage(name: ReagentsName){
+fun buildLevel1(resources:Resources): ArrayList<String> {
+    var xrp:XmlResourceParser = resources.getXml(R.xml.lvl1)
+    var reactives= ArrayList<String>()
+    var products=ArrayList<String>()
+    while (xrp.eventType!=XmlResourceParser.END_DOCUMENT){
+        if(xrp.eventType==XmlResourceParser.START_TAG) {
+            if (xrp.name == "Reagent") {
+                xrp.next()
+                reactives.add(xrp.text.toString())
+            }
+            if (xrp.name == "Product") {
+                xrp.next()
+                products.add(xrp.text.toString())
+            }
+        }
+        Log.d("БЛЯТЬ","${xrp.name}")
+        xrp.next()
     }
+    return reactives
 }
 
 class MainActivity : AppCompatActivity() {
@@ -30,6 +46,12 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this,LevelsChoose::class.java)
             startActivity(intent)
         }
+        var arr = buildLevel1(resources)
+        if(arr.isEmpty()){
+            Log.d("БЛЯТЬ","ПИЗДЕЦ")
+        }
+        val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arr)
+        item_list.adapter = adapter
     }
 
     var check = 100

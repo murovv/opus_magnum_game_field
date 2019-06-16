124c7dcedefdffca1696f4d73385234fb68785d3
murovv <murovgleb@gmail.com>
2019-06-16 18:43:59 +0300

    начало дезигна и начало итемов

--- app/src/main/java/com/example/opus_magnum_game_field/MainMenu.kt
+++ app/src/main/java/com/example/opus_magnum_game_field/MainMenu.kt
@@ -7,13 +7,13 @@ import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.MainMenu.*
+import kotlinx.android.synthetic.main.main_menu.*
 
 class MainMenu : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.MainMenu)
+        setContentView(R.layout.main_menu)
 
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE //horizontal app
         background_layout.background = BitmapDrawable(resources,BitmapFactory.decodeResource(resources,R.drawable.background))

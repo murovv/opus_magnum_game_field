50036a620b260103fa6644fdf70cd7e0047ddd25
murovv <murovgleb@gmail.com>
2019-06-17 15:04:34 +0300

    начато выделение игроком элементов игры

--- app/src/main/java/com/example/opus_magnum_game_field/GameField.kt
+++ app/src/main/java/com/example/opus_magnum_game_field/GameField.kt
@@ -18,6 +18,8 @@ import com.example.opus_magnum_game_field.Objects.Element
 import kotlinx.android.synthetic.main.activity_main.*
 import android.view.ViewGroup
 import android.widget.*
+import com.example.opus_magnum_game_field.Objects.Operators
+import com.example.opus_magnum_game_field.Objects.Reagents
 
 
 fun buildLevel1(resources:Resources): ArrayList<String> {
@@ -35,7 +37,6 @@ fun buildLevel1(resources:Resources): ArrayList<String> {
                 products.add(xrp.text.toString())
             }
         }
-
         xrp.next()
     }
     return reactives
@@ -54,11 +55,36 @@ fun listSizeCheck( listView: ListView): LinearLayout.LayoutParams {
     }
 
     return params
-
 }
 
 
 class MainActivity : AppCompatActivity() {
+    fun getReactive(name:String,resources: Resources){
+        var xrp = resources.getXml(R.xml.lvl1)
+        var cost:Int=0
+        var numOfCells:Int = 0
+        while(xrp.eventType!=XmlResourceParser.END_DOCUMENT){
+            if(xrp.name=="Reagent" && xrp.getAttributeValue(null,"name")==name){
+                val amount:Int = xrp.getAttributeValue(null,"amount").toInt()
+                cost = xrp.getAttributeValue(null,"cost").toInt()
+                numOfCells= xrp.getAttributeValue(null,"numOfCells").toInt()
+                break
+            }
+            xrp.next()
+        }
+        var reagent: Reagents = Reagents(this,cost,name, Array(2) {0},30,numOfCells)
+    }
+    fun getManipulator(name:String,resources: Resources){
+
+    }
+    fun getProduct(){
+
+    }
+    fun getOperator(){
+
+    }
+    var chosenElement: Element? = null //TODO Нужно, чтобы эта переменная принимала значения элемента, соответствующего нажатой кнопке
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -117,6 +143,10 @@ class MainActivity : AppCompatActivity() {
             productList.setLayoutParams(listSizeCheck(productList))
         }
         operatorList.setOnItemClickListener{parent, view, position, id ->
+            var textView: TextView = view as TextView
+            choosen_item.text = textView.text.toString()
+        }
+        manipulatorList.setOnItemClickListener{parent, view, position, id->
             var textView: TextView = view as TextView
             choosen_item.text = textView.text.toString()
         }

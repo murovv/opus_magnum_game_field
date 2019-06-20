065565a4e4fb1880eb09f3b12465172cc33561f9
egorsergeev <noartu@gmail.com>
2019-06-20 14:12:21 +0300

    чёрный текст в меню/noartu

--- app/src/main/java/com/example/opus_magnum_game_field/GameField.kt
+++ app/src/main/java/com/example/opus_magnum_game_field/GameField.kt
@@ -10,7 +10,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.TextView
